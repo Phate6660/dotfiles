47e5bc30f6f77bc9139f8fb39cff240a93ab4897
Ash <44369810+Phate6660@users.noreply.github.com>
2019-05-16 14:34:10 -0400

    Update xmonad.hs

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -27,6 +27,14 @@ import Data.Tree
 import XMonad.Actions.TreeSelect
 import XMonad.Hooks.WorkspaceHistory
 import XMonad.StackSet as W
+import XMonad.Hooks.UrgencyHook
+import XMonad.Util.NamedWindows
+import XMonad.Util.Run
+import Data.Maybe (fromMaybe)
+import XMonad.Actions.ShowText
+import XMonad.Util.Loggers (logCurrent)
+import XMonad.Util.Dzen as D
+import XMonad.Util.Font (Align(..))
 
 ------------
 -- Variables
@@ -44,6 +52,7 @@ myManageHook = composeAll
    , className =? "gimp"  --> doFloat
    , className =? "rld2-alt" --> doFloat
    , className =? "gzdoom"  --> doFloat
+   , className =? "Xwinmosaic" --> doFloat
    , isFullscreen --> doFloat
    , className =? "mpv" --> hasBorder False
    , className =? "mpv" --> doFullFloat
@@ -73,6 +82,8 @@ myScratchPads = [
         t = 0.95 -h
         l = 0.95 -w
 
+term = "-*-Misc Termsynu-*-*-*-*-12-*-*-*-*-*-*-*"
+
 --------------
 -- Main Config
 --------------
@@ -88,32 +99,33 @@ main = xmonad $ baseConfig {
     , ("M-S-f", spawn "filezilla") -- (S)FTP File Manager
     , ("M-w", spawn "firefox-nightly") -- Web Browser
     , ("M-t", spawn "urxvt") -- Terminal
-    , ("M-S-g", spawn "gimp") -- Image Editor
+    , ("M-S-g", spawn "gimp-2.99") -- Image Editor
     , ("M-S-r", spawn "rld2-alt") -- DOOM
     , ("M-b", spawn "blender") -- 3D Modeling
-    , ("M-v", spawn "vpnstat") -- VPN Info
+    , ("M-S-b", spawn "bwmenu") -- Biwarden - Rofi Edition
+    , ("M-v", spawn "vpnstat | dzen2 -p 3 -fg \"#FFFFFF\" -bg \"#0000d0\" -h 12 -fn \"Misc Termsynu\"") -- VPN Info
     , ("<Print>", spawn "gnome-screenshot -i") -- Screenshot
     , ("M-M1-m", spawn "mc") -- Minecraft
-    , ("C-q", spawn "notify-send.sh \"Oops.\" \"You almost quit your program.\nPlease use [Ctrl-Shift-q].\"") -- Application Quit Warning
+    , ("C-q", spawn "echo \"Oops. You almost quit your program. Please use [Alt-F4].\" | dzen2 -p 3 -fg \"#FFFFFF\" -bg \"#0000d0\" -h 12 -fn \"Misc Termsynu\"") -- Application Quit Warning
     , ("M-l", spawn "notify-lyrics") -- Send notification with lyrics to currently playing song (mpd).
     , ("C-M1-<Space>", spawn "rofi -show run") -- Rofi
-    , ("M-n", spawn "music-notify") -- Send notification with info on currently playing song.
+    , ("M-S-n", spawn "rofi-stuff") -- Menu system in Rofi.
     , ("<XF86AudioPlay>", spawn "mpc toggle") -- Play/Pause music (mpd).
     , ("<XF86AudioStop>", spawn "mpc stop") -- Stop music (mpd).
     , ("<XF86AudioNext>", spawn "mpc next") -- Next song in playlist (mpd).
     , ("<XF86AudioPrev>", spawn "mpc prev") -- Previous song in playlist (mpd).
     , ("<XF86AudioMute>", spawn "pactl set-sink-mute @DEFAULT_SINK@ toggle") -- Mute/Un-Mute Volume (pulseaudio).
-    , ("<XF86AudioLowerVolume>", spawn "sh -c \"pactl set-sink-mute @DEFAULT_SINK@ false ; pactl set-sink-volume @DEFAULT_SINK@ -5%\"") -- Volume -5% (pulseaudio).
-    , ("<XF86AudioRaiseVolume>", spawn "sh -c \"pactl set-sink-mute @DEFAULT_SINK@ false ; pactl set-sink-volume @DEFAULT_SINK@ +5%\"") -- Volume +5% (pulseaudio).
+    , ("<XF86AudioLowerVolume>", spawn "sh -c \"pactl set-sink-mute @DEFAULT_SINK@ false ; pactl set-sink-volume @DEFAULT_SINK@ -5% ; /home/valley/Scripts/volume\"") -- Volume -5% (pulseaudio).
+    , ("<XF86AudioRaiseVolume>", spawn "sh -c \"pactl set-sink-mute @DEFAULT_SINK@ false ; pactl set-sink-volume @DEFAULT_SINK@ +5% ; volume\"") -- Volume +5% (pulseaudio).
     , ("M-S-s", spawn "zentray") -- Systray in zenity.
     , ("M-s", spawn "urxvt -e tmux attach -t scli") -- Spawn a new urxvt window and attach scli window from tmux.
-    , ("M-d", spawn "time-date") -- Send notification of time and date.
-    , ("M-S-d", spawn "dunst-start") -- Start dunst.
+    , ("M-d", spawn "date +\"%I:%M %p | %A %d, %B of %Y\" | dzen2 -p 3 -fg \"#FFFFFF\" -bg \"#0000d0\" -h 12 -fn \"Misc Termsynu\"") -- Send notification of time and date.
     , ("M-S-m", spawn "openmw-launcher") -- OpenMW Launcher
     , ("M-m", spawn "openmw") -- OpenMW
-    , ("M-S-n", spawn "neo-notify") -- Neofetch (ufetch) notification.
-    , ("M-u", spawn "unclutter --timeout 3 --fork") -- Automatically hide mouse after 3 seconds of inactivity.
+    , ("M-n", spawn "music-notify | dzen2 -p 7 -fg \"#FFFFFF\" -bg \"#0000d0\" -h 12 -fn \"Misc Termsynu\"") -- dzen message for music info..
+    , ("M-u", spawn "echo -e \"You have $(checkupdates+aur | wc -l) pending updates.\n\n$(checkupdates+aur)\" | dzen2 -l 10 -p 7 -fg \"#FFFFFF\" -bg \"#0000d0\" -h 12 -fn \"Misc Termsynu\" -sa c") -- Send notification of pending updates.
     , ("M-c", spawn "rofi -modi \"clipboard:greenclip print\" -show clipboard -run-command '{cmd}'") -- View clipboard.
+    , ("M-S-c", spawn "rofi -show calc -modi calc -no-show-match -no-sort -no-history")
     , ("M-r", spawn "xmonad --recompile; xmonad --restart") -- Restart xnomad.
     , ("C-M1-<Delete>", io (exitWith ExitSuccess)) -- Exit xnomad.
     , ("M-<Up>", sendMessage $ Go U) -- Win + Up moves focus to window above current window.
@@ -126,7 +138,8 @@ main = xmonad $ baseConfig {
     , ("M-S-<Right>", sendMessage $ Swap R) -- Win + Right swaps window with the one to right of current window.
     , ("M1-<F4>", kill) -- Kill focused window.
     , ("M-x", spawn "xscreensaver-command -lock") -- Lock the screen.
-    , ("C-M1-<Left>", prevWS) -- Go to previous workspace.
-    , ("C-M1-<Right>", nextWS) -- Go to next workspace.
+    , ("C-M1-<Left>", prevWS >> logCurrent >>= dzenConfig (D.fgColor "#FFFFFF" >=> D.bgColor "#0000d0" >=> font term >=> timeout (0.25)) . fromMaybe "") -- Go to previous workspace, and send dzen notification of which workspace you went to.
+    , ("C-M1-<Right>", nextWS >> logCurrent >>= dzenConfig (D.fgColor "#FFFFFF" >=> D.bgColor "#0000d0" >=> font term >=> timeout (0.25)) . fromMaybe "") -- Go to next workspace, and send dzen notification of which workspace you went to.
     , ("M-`", namedScratchpadAction myScratchPads "terminal") -- Terminal Scratchpad.
+    , ("M1-<Tab>", spawn "xwinmosaic") -- Window Switcher
     ]
