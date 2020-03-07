15699f67420412da2ed1778615f70ba7d1882877
Ash <ValleyKnight@protonmail.com>
2020-03-07 18:00:56 -0500

    update scripts

--- scripts/ips.sh
+++ scripts/ips.sh
@@ -1,19 +1,17 @@
 #!/bin/sh
 
 # This script display external and internal IP address in two rows
-# Depends on `bind-tools` and `protonvpn` (https://github.com/ProtonVPN/protonvpn-cli-ng)
+# Depends on `bind-tools`
 
 # Author: Piotr Miller
 # e-mail: nwg.piotr@gmail.com
 # Website: http://nwg.pl
 # Project: https://github.com/nwg-piotr/tint2-executors
 # License: GPL3
-# Modified by: Phate6660 (https://github.com/Phate6660)
 
-w="$(dig +short myip.opendns.com @resolver1.opendns.com)"
-# Replace [REDACTED] with your typical IP when the VPN is off.
-if [ "$w" = "[REDACTED]" ]; then
-    output="| VPN: OFF | $w |"
+i="$(dig +short myip.opendns.com @resolver1.opendns.com)"
+if protonvpn s | grep Disconnected; then
+    output="| VPN: OFF | $i |"
 else
     s="$(protonvpn s | sed -n 4p | awk -F\  '{ print $2 }')"
     f="$(protonvpn s | sed -n 5p | awk -F\  '{ print $2 }')"
@@ -24,6 +22,6 @@ else
         "Secure-Core") f="SC";;
         *) f="?";;
     esac
-    output="| VPN: ON ($s - $f) | $w |"
+    output="VPN: ON ($s - $f) | $i"
 fi
 echo "$output"
